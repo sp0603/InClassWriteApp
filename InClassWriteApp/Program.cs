ff695540df35df8deca6a7d120a512dd97e4d3be
Sydney Porter <Syd.porter03@gmail.com>
2026-03-17 19:49:05 -0400

    In class assignment that uses ReadLine and WriteLine

---
+++ InClassWriteApp/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Please enter your name:");
+
+string firstName = Console.ReadLine();
+
+Console.WriteLine("\nHello " + firstName + "! Please enter the word you want to determine if it's a palindrome!\n");
+
+string word = Console.ReadLine();
+
+var isPalindrome = true;
+
+char[] firstString = new char[word.Length];
+char[] secondString = new char[word.Length];
+
+for (int i = 0; i < word.Length; i++)
+{
+    firstString[i] = word[i];
+}
+
+int counter = 0;
+for (int j = word.Length - 1; j >= 0; j--)
+{
+    secondString[counter] = word[j];
+    counter++;
+}
+
+
+bool areEqual = firstString.SequenceEqual(secondString);
+// Console.WriteLine(firstString);
+// Console.WriteLine(areEqual);
+
+if (areEqual)
+{
+    Console.WriteLine("\nOoooo, it's perfect!");
+}
+
+else
+{
+   Console.WriteLine("\nLook at the word reversed: " + new string(secondString) + "\nVery ugly if I say so myself. Yuck!");
+ 
+}
